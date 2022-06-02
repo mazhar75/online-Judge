f3afc31414fdfb082f454a3d7b0a5ef967641e94
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:34:03 +0600

    Committing files

---
+++ CodeChef/C++17/S01E01/54496487.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+
+    int n;
+    cin>>n;
+    if(n>=21){
+        cout<<"YES"<<endl;
+
+    }
+    else
+    cout<<"NO"<<endl;
+}
+
+return 0;
+}
