e7d2dc15c4f014c3a2ccee7d1438be5ada95201d
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:44:09 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1638A/146418108.cpp
@@ -0,0 +1,91 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<cmath>
+#include<algorithm>
+
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#include<utility>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e2+10;
+const int MAXN = 100010;
+
+
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+
+
+};
+void cp::think(){
+int n;
+cin>>n;
+vector<int>a(n);
+for(int i=0;i<n;i++) cin>>a[i];
+int l=0,k=0,flag=0;
+
+for(int i=0;i<n;i++){
+ if(a[i]==i+1){
+ flag=1;
+ continue;
+ }
+ else{
+ flag=0;
+ l=i;
+ break;
+ }
+}
+if(flag==1){
+  for(int i=0;i<n;i++)
+   {cout<<a[i]<<" ";
+    }
+    cout<<endl;
+   return;
+}
+for(int i=l+1;i<n;i++){
+  if(a[i]==l+1){
+  k=i;
+  break;
+  }
+}
+//cout<<l<<" "<<k<<endl;
+reverse(a.begin()+l,a.begin()+1+k);
+for(int i=0;i<n;i++)
+cout<<a[i]<<" ";
+cout<<endl;
+
+
+}
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+  cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
