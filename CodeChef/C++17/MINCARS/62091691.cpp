62b9d622f9c06b1d3edbcc33f6d81997a15ee318
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:32:48 +0600

    Committing files

---
+++ CodeChef/C++17/MINCARS/62091691.cpp
@@ -0,0 +1,63 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <bits/stdc++.h>
+#include<stack>
+#define endl "\n"
+
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e9;
+const int MAXN = 100010;
+
+
+class cp
+{
+public:
+    void think(int);
+};
+
+bool isPowerOfTwo(ll n)
+{
+    return ((n)&(n-1)); // if 0 than yes else false
+}
+ll PowerOfTwo(int n)
+{
+    return 1<<n;
+
+}
+
+
+void cp::think(int o)
+{
+    int n;
+    cin>>n;
+    int k=n/4;
+    if(k*4==n)
+    cout<<k<<endl;
+    else
+    cout<<(k+1)<<endl;
+
+}
+
+
+
+int main()
+{
+
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cp me;
+    int t,i=1;
+    cin>>t;
+    while(t--)
+    {
+
+        me.think(i);
+        i++;
+
+    }
+    return 0;
+}
