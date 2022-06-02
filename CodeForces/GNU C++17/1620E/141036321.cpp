b0a0e6a55d91ce9781ec3f76dd61e2fe6505f1ad
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:52:42 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1620E/141036321.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 500 * 1000 + 13;
+
+int main() {
+  int q;
+  scanf("%d", &q);
+  vector<int> t(q), x(q), y(q);
+  for (int i = 0; i < q; ++i) {
+    scanf("%d%d", &t[i], &x[i]);
+    if (t[i] == 2) scanf("%d", &y[i]);
+  }
+  vector<int> p(N);
+  iota(p.begin(), p.end(), 0);
+  /**for(auto it=x.begin();it!=x.end();it++){
+    cout<<*it<<" ";
+  }
+  cout<<endl;
+  for(auto it=y.begin();it!=y.end();it++){
+    cout<<*it<<" ";;
+  }
+  cout<<endl;*/
+
+  vector<int> ans;
+  for (int i = q-1; i >=0; --i) {
+    if (t[i] == 1) {
+      ans.push_back(p[x[i]]);
+      //cout<<p[x[i]]<<" ";
+    } else {
+      p[x[i]] = p[y[i]];
+    }
+  }
+  //cout<<endl;
+  reverse(ans.begin(), ans.end());
+  for (int &x : ans) printf("%d ", x);
+  return 0;
+}
