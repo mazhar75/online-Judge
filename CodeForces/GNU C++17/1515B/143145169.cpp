59f648444e3dfa532afe580b64089297705e8c0a
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:51:45 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1515B/143145169.cpp
@@ -0,0 +1,63 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+bool isSquare(int x){
+  int y=sqrt(x);
+  return y*y==x;
+}
+
+
+
+void cp::think(){
+  int n;
+  cin>>n;
+  if (n%2==0 && isSquare(n/2))
+    cout<<"YES"<<endl;
+  else if (n%4==0 && isSquare(n/4))
+    cout<<"YES"<<endl;
+  else
+    cout<<"NO"<<endl;
+  }
+
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+ cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
