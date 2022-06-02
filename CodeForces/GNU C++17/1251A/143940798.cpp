48076405f736f9a55f4a5bfe634ce195be424a44
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:50:33 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1251A/143940798.cpp
@@ -0,0 +1,82 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#include<utility>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+bool isSquare(int x){
+  int y=sqrt(x);
+  return y*y==x;
+}
+int countOdd(int L, int R){
+
+    int N = (R - L) / 2;
+
+
+    if (R % 2 != 0 || L % 2 != 0)
+        N += 1;
+
+    return N;
+}
+
+
+
+void cp::think(){
+string s;
+cin>>s;
+int l=s.length();
+set<char>sc;
+for(int i=0;i<l;i++){
+  if(s[i]==s[i+1]) i++;
+  else{
+  sc.insert(s[i]);
+  }
+
+}
+for(auto it=sc.begin();it!=sc.end();it++)
+cout<<*it;
+cout<<endl;
+
+
+
+}
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+  cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
