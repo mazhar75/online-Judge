fc4c9ffcb88f186e09d12073eac13be48c4c8664
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:41:15 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1658B/151142421.cpp
@@ -0,0 +1,81 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <bits/stdc++.h>
+#include<stack>
+#define endl "\n"
+#define m 998244353
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e9;
+const int MAXN = 100010;
+
+class cp
+{
+public:
+    void think(int);
+};
+
+bool isPowerOfTwo(ll n)
+{
+    return ((n)&(n-1)); // if 0 than yes else false
+}
+ll PowerOfTwo(int n)
+{
+    return 1<<n;
+
+}
+ll fact(ll n)
+{
+    if (n >= m)
+        return 0;
+
+    ll result = 1;
+    for (int i = 1; i <= n; i++)
+        result = (result * i) % m;
+
+    return result;
+}
+
+void cp::think(int o)
+{
+    ll n;
+    cin>>n;
+    if(n%2!=0)cout<<"0\n";
+    else
+    {
+        ll d=fact(n/2);
+        d=((d%m)*(d%m))%m;
+
+        cout<<d<<endl;
+
+    }
+
+
+
+}
+
+
+
+
+
+
+int main()
+{
+
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cp me;
+    int t,i=1;
+    cin>>t;
+    while(t--)
+    {
+
+        me.think(i);
+        i++;
+
+    }
+    return 0;
+}
