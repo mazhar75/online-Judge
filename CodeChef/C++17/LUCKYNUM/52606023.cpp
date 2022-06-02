9adce04cac5ecfc7ea9af31b50fb3e873fea7f01
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:34:09 +0600

    Committing files

---
+++ CodeChef/C++17/LUCKYNUM/52606023.cpp
@@ -0,0 +1,20 @@
+/**Bismillah
+Author:Md.mazharul Islam
+SUST*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a==7)||(b==7)||(c==7))
+            cout<<"YES"<<endl;
+            else
+                cout<<"NO"<<endl;
+    }
+    return 0;
+}
