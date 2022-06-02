31d8b60d68d98d08ddbd2f208479be610c4b5684
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:33:06 +0600

    Committing files

---
+++ CodeChef/C++17/JGAMES/59668698.cpp
@@ -0,0 +1,51 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e6+10;
+const int MAXN = 100010;
+
+
+
+
+
+
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+};
+
+
+
+
+
+void cp::think(){
+int x,y;
+cin>>x>>y;
+if(x%2==0 and y%2==0){
+cout<<"Janmansh"<<endl;
+return;
+}
+if(x%2!=0 and y%2!=0){
+cout<<"Janmansh"<<endl;
+return;
+}
+cout<<"Jay"<<endl;
+
+}
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+  cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
