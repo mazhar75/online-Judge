561e45032908d593ab2a88f0f68a67e029ec9e44
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:51:47 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1626A/142973069.cpp
@@ -0,0 +1,56 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+
+
+
+void cp::think(){
+
+	string s;
+	cin>>s;
+	sort(s.begin(),s.end());
+	cout<<s<<endl;
+
+
+
+  }
+
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+ cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
