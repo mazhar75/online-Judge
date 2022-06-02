06e76e9a27efc676a84660cc9cf8563dce1a822f
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:51:04 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1613A/143512201.cpp
@@ -0,0 +1,69 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+
+
+
+void cp::think(){
+long long x1, x2;
+		int p1, p2;
+		cin >> x1 >> p1 >> x2 >> p2;
+		int mn = min(p1, p2);
+		p1 -= mn;
+		p2 -= mn;
+		if (p1 >= 7)
+			cout << ">" << endl;
+		else if (p2 >= 7)
+			cout << "<" << endl;
+		else{
+			for (int i = 0; i < p1; ++i) x1 *= 10;
+			for (int i = 0; i < p2; ++i) x2 *= 10;
+			if (x1 < x2)
+				cout << "<" << endl;
+			else if (x1 > x2)
+				cout << ">" << endl;
+			else
+				cout << "=" << endl;
+}}
+
+
+
+
+
+
+int main() {
+    RUN_FAST;
+    cp me;
+  int t;
+ cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
