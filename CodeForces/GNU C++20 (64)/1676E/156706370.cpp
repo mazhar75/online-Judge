de7cc6caa6b4385abf9ee8453b703e44f2ee5d1c
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:33:28 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1676E/156706370.cpp
@@ -0,0 +1,129 @@
+// BISMILLAH
+
+//Never give up.Slow and steady wins the race
+// Author:Md.Mazharul Islam
+
+#include <bits/stdc++.h>
+
+
+
+#define endl "\n"
+#define rr(i, a, b) for(long long int i=a; i<b; i++)
+#define vin(n, a) vector <long long int> a(n); rr(i, 0, n) cin >> a[i]
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define rn return
+#define pi 3.14159265
+
+
+
+using namespace std;
+typedef long long int ll;
+const int mxN=100;
+
+
+class cp
+{
+public:
+    void think(int);
+};
+
+bool isPowerOfTwo(ll n)
+{
+    return ((n)&(n-1)); // if 0 than yes else false
+}
+ll PowerOfTwo(int n)
+{
+    return 1<<n;
+}
+
+
+
+
+int func(int x)
+{
+    int ans=1e9;
+    for(int i=0; i<=16; i++)
+    {
+        ans=min(ans, 15+i-(__builtin_ctz(x+i)));
+        //cout<<(__builtin_ctz(x+i))<<" ";
+    }
+    return ans;
+
+}
+bool isPalindrome(string s)
+{
+    string r=s;
+    reverse(s.begin(),s.end());
+    int l=s.length();
+    bool flag=true;
+    for(int i=0; i<l; i++)
+    {
+        if(s[i]!=r[i])
+        {
+            flag=false;
+            break;
+        }
+    }
+    return flag;
+
+}
+//Main Code
+
+
+void cp::think(int o)
+{
+   int n,q;
+   cin>>n>>q;
+   vector<int>v(n),p(n);
+
+   for(int i=0;i<n;i++){
+       cin>>v[i];
+   }
+  sort(v.begin(),v.end(),greater <>());
+  p[0]=v[0];
+    for(int i=1;i<n;i++){
+        p[i]=v[i]+p[i-1];
+    }
+  while(q--){
+      int x;
+      cin>>x;
+      auto lower=lower_bound(p.begin(),p.end(),x);
+      if(lower!=p.end()){
+      cout<<lower-p.begin()+1<<'\n';
+      }
+      else{
+          cout<<-1<<'\n';
+      }
+  }
+
+
+
+
+
+}
+
+
+
+
+
+
+int main()
+{
+
+    //freopen("00_input.txt","r+",stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cp me;
+    int t,i=1;
+    cin>>t;
+    while(i<=t)
+    {
+
+        me.think(i);
+        i++;
+
+    }
+    return 0;
+}
