be65754e4161f36400f2d9706e6e7bee23c45a15
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:35:22 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1505A/153363111.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define flush fflush(stdout)
+
+int main() {
+    string in;
+    while (getline(cin, in)) {
+        printf("NO\n");
+        flush;
+    }
+    
+    return 0;
+}
