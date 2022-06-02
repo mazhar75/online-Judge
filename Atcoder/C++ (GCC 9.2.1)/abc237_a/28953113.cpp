af249864aff0d354b42ee22e5786cdd095a564c6
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:32:45 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc237_a/28953113.cpp
@@ -0,0 +1,59 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#include<utility>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e2+10;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+
+
+
+void cp::think(){
+ll n;
+cin>>n;
+ll k=-2147483648,j=2147483647;
+if(n<k or n>j)
+cout<<"No"<<endl;
+else
+cout<<"Yes"<<endl;
+}
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t=1;
+  //cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
