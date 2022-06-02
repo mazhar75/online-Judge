da8720eae1f98664497f1c0363ad79cc4b886b65
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:51:40 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1506B/143161268.cpp
@@ -0,0 +1,71 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+bool isSquare(int x){
+  int y=sqrt(x);
+  return y*y==x;
+}
+
+
+
+void cp::think(){
+int n, k;
+  cin >> n >> k;
+  string s;
+  cin >> s;
+  int res = 1;
+  int i = s.find_first_of('*');
+  while (true) {
+    int j = min(n - 1, i + k);
+    for (; i < j && s[j] == '.'; j--) {}
+    if (i == j) {
+      break;
+    }
+    res++;
+    i = j;
+  }
+  cout << res << "\n";}
+
+
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+ cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
