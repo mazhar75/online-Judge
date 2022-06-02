f3dff58cb310145ec0fb12b273687facc85a4361
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:32:42 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc236_a/28724353.cpp
@@ -0,0 +1,75 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#include<utility>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+bool isSquare(int x){
+  int y=sqrt(x);
+  return y*y==x;
+}
+int countOdd(int L, int R){
+
+    int N = (R - L) / 2;
+
+
+    if (R % 2 != 0 || L % 2 != 0)
+        N += 1;
+
+    return N;
+}
+
+
+
+void cp::think(){
+string s;
+cin>>s;
+int a,b;
+cin>>a>>b;
+swap(s[a-1],s[b-1]);
+cout<<s<<endl;
+
+
+
+
+}
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t=1;
+ //cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
