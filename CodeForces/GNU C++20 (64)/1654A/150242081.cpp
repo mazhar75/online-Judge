0df95dfccdf59d9c0962e3c4b54a3d4f1af42e64
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:41:40 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1654A/150242081.cpp
@@ -0,0 +1,71 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <bits/stdc++.h>
+#include<stack>
+#define endl "\n"
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e9;
+const int MAXN = 100010;
+
+class cp
+{
+public:
+    void think(int);
+};
+
+bool isPowerOfTwo(ll n)
+{
+    return ((n)&(n-1)); // if 0 than yes else false
+}
+ll PowerOfTwo(int n)
+{
+    return 1<<n;
+
+}
+
+
+void cp::think(int x)
+{
+  ll n;
+  cin>>n;
+  vector<ll>a(n);
+  for(int i=0;i<n;i++)cin>>a[i];
+  ll sum=0;
+  sort(a.begin(),a.end());
+  for(int i=n-1;i>n-3;i--){
+  sum+=a[i];
+  }
+  cout<<sum<<endl;
+
+
+
+
+}
+
+
+
+
+
+
+
+int main()
+{
+
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cp me;
+    int t,i=1;
+    cin>>t;
+    while(t--)
+    {
+
+        me.think(i);
+        i++;
+
+    }
+    return 0;
+}
