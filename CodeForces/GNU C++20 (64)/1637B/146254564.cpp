c0f514662e8b4c63aec8fa96c69fd4bc3e2281b2
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:44:11 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1637B/146254564.cpp
@@ -0,0 +1,70 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<cmath>
+#include<algorithm>
+
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#include<utility>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+const int N =1e2+10;
+const int MAXN = 100010;
+
+
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+
+
+};
+void cp::think(){
+int n;
+cin>>n;
+vector<int>a(n);
+for(int i=0;i<n;i++) cin>>a[i];
+
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (i + 1) * (n - i);
+            if (a[i] == 0)
+                ans += (i + 1) * (n - i);
+        }
+        cout << ans << '\n';
+
+
+
+}
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+  cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
