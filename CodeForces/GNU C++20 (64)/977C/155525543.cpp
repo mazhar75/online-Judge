5d48058adb93fbc3688283171442ce82482fee5d
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:34:14 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/977C/155525543.cpp
@@ -0,0 +1,150 @@
+// BISMILLAH
+
+//Never give up.Slow and steady wins the race
+// Author:Md.Mazharul Islam
+
+#include <bits/stdc++.h>
+
+
+
+#define endl "\n"
+#define rr(i, a, b) for(long long int i=a; i<b; i++)
+#define vin(n, a) vector <long long int> a(n); rr(i, 0, n) cin >> a[i]
+#define yes cout<<"YES"<<"\n"
+#define no cout<<"NO"<<"\n"
+#define rn return
+#define pi 3.14159265
+
+
+
+using namespace std;
+typedef long long int ll;
+const int mxN=100;
+/**typedef pair<int,int>pii;
+const int N =1e9;
+const int MAXN = 100010;
+int cnt;
+*/
+
+class cp
+{
+public:
+    void think(int);
+};
+
+bool isPowerOfTwo(ll n)
+{
+    return ((n)&(n-1)); // if 0 than yes else false
+}
+ll PowerOfTwo(int n)
+{
+    return 1<<n;
+}
+
+
+
+
+int func(int x)
+{
+    int ans=1e9;
+    for(int i=0; i<=16; i++)
+    {
+        ans=min(ans, 15+i-(__builtin_ctz(x+i)));
+        //cout<<(__builtin_ctz(x+i))<<" ";
+    }
+    return ans;
+
+}
+bool isPalindrome(string s)
+{
+    string r=s;
+    reverse(s.begin(),s.end());
+    int l=s.length();
+    bool flag=true;
+    for(int i=0; i<l; i++)
+    {
+        if(s[i]!=r[i])
+        {
+            flag=false;
+            break;
+        }
+    }
+    return flag;
+
+}
+//Main Code
+
+
+
+void cp::think(int o)
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    map<int,int>mp;
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+        mp[a[i]]++;
+    }
+    sort(a.begin(),a.end());
+    if(k==0)
+    {   if(a[0]!=1)
+        cout<<(a[0]-1)<<endl;
+        else cout<<-1<<endl;
+        return;
+    }
+    if(k==1)
+    {
+        if(n==1)
+        {
+            cout<<a[0]<<endl;
+            return;
+        }
+        else if(a[0]!=a[1])
+        {
+            cout<<a[0]<<endl;
+            return;
+        }
+        else cout<<-1<<endl;
+        return;
+    }
+    if(k==n)
+    {
+        cout<<a[n-1]<<endl;
+        return;
+    }
+    if(a[k]!=a[k-1])
+    {   if(a[k]-1==a[k-1])
+        cout<<a[k-1]<<endl;
+        else cout<<++a[k-1]<<endl;
+        return;
+    }
+    cout<<-1<<endl;
+
+
+
+}
+
+
+
+
+int main()
+{
+
+    //freopen("input.txt","r+",stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cp me;
+    int t=1,i=1;
+    //cin>>t;
+    while(i<=t)
+    {
+
+        me.think(i);
+        i++;
+
+    }
+    return 0;
+}
