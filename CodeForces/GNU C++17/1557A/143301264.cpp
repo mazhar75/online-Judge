2a6d99924defd6079bf247ae0d689e73b5412bac
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:51:19 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1557A/143301264.cpp
@@ -0,0 +1,66 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include <cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#include<map>
+#include<sstream>
+#include<string>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+//#define REP(i, a, b) for (long long int i=a; i<b; i++)
+//#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+bool isSquare(int x){
+  int y=sqrt(x);
+  return y*y==x;
+}
+
+
+
+void cp::think(){
+int n;
+cin>>n;
+vector<ll>vec(n);
+for(int i=0;i<n;i++) cin>>vec[i];
+sort(vec.begin(),vec.end());
+ll sum=0;
+for(int i=0;i<n-1;i++) sum+=vec[i];
+double x=sum*1.0;
+
+double avg=x/(n-1)+vec[n-1]*1.0;
+printf("%.10lf\n",avg);
+
+}
+
+
+
+
+
+
+int main() {
+    //RUN_FAST;
+    cp me;
+  int t;
+ cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
