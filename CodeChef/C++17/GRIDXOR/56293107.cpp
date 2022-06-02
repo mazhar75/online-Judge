4b9d6277dff0ff4b11e38b893b4039e217160f84
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:33:41 +0600

    Committing files

---
+++ CodeChef/C++17/GRIDXOR/56293107.cpp
@@ -0,0 +1,52 @@
+/** Never give up.slow and steady wins the race*/
+/** author:Md.Mazharul Islam*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<queue>
+#include<cstdio>
+#include <cstdlib>
+#include<set>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int>pii;
+
+#define REP(i, a, b) for (long long int i=a; i<b; i++)
+#define RUN_FAST ios::sync_with_stdio(false);
+class cp{
+public:
+void think();
+void binary();
+};
+
+void cp::think(){
+ll n;
+cin>>n;
+for(int i=0;i<n;i++){
+  for(int j=0;j<n;j++)
+    {
+    cout<<1<<" ";
+    }
+    cout<<endl;
+}
+
+     }
+
+
+
+
+int main() {
+    RUN_FAST;
+    cp me;
+  int t;
+ cin>>t;
+  while(t--){
+    me.think();
+  }
+  return 0;
+}
