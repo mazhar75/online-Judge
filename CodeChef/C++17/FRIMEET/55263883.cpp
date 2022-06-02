b0effb1620f47221579241f02d82416a6b26171e
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:34:16 +0600

    Committing files

---
+++ CodeChef/C++17/FRIMEET/55263883.cpp
@@ -0,0 +1,27 @@
+
+/** Author:Md.Mazharul Islam
+CSE,SUST*/
+#include <bits/stdc++.h>
+
+#define endl  "\n"
+using namespace std;
+
+
+
+
+int main()
+{   int t;
+cin>>t;
+  while(t--){
+        int x1,x2;
+        cin>>x1>>x2;
+        if(x1>=x2)
+            cout<<"YES"<<endl;
+            else
+                cout<<"NO"<<endl;
+
+
+  }
+
+    return 0;
+}
