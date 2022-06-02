43e26a0aadfc466c62b9a254571ee4f3423deac5
Md.Mazharul Islam <70192123+mazhar75@users.noreply.github.com>
2022-06-03 01:50:14 +0600

    Committing files

---
+++ CodeForces/Java 11/1584A/144339059.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class solution{
+	
+ public static void main(String[] args){
+
+ Scanner sc=new Scanner(System.in);
+ long t=sc.nextLong();
+ long u,v,x,y;
+ while(t>0){
+ u=sc.nextLong();
+ v=sc.nextLong();
+ x=-u*u;
+ y=v*v;
+ System.out.println(x+" "+y);
+ t--;
+
+ }
+ }
+
+}
